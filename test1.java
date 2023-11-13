11356adfe82812fe2c1cf9628d2f8bb037d4707f
Ubuntu <ubuntu@ip-172-31-53-240.ec2.internal>
2023-11-13 10:41:01 +0000

    add test2.java

--- test1.java
+++ test1.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.util.Scanner;
 public class Dec2Hex {
     public static void main(String args[]) {
@@ -27,4 +28,37 @@ public class Dec2Hex {
         }
         System.out.println("Hexadecimal representation is: " + (hexadecimal.isEmpty() ? "0" : hexadecimal));
     }
+=======
+
+
+// Java Program to find the if the arrays are equal 
+  
+import java.util.Arrays; 
+  
+public class CheckArraysEqual { 
+    public static void main(String[] args) 
+    { 
+        // Initializing the first array 
+        int a[] = { 30, 25, 40 }; 
+  
+        // Initializing the second array 
+        int b[] = { 30, 25, 40 }; 
+  
+        // store the result 
+        // Arrays.equals(a, b) function is used to check 
+        // whether two arrays are equal or not 
+        boolean result = Arrays.equals(a, b); 
+  
+        // condition to check whether the 
+        // result is true or false 
+        if (result == true) { 
+            // Print the result 
+            System.out.println("Two arrays are equal"); 
+        } 
+        else { 
+            // Print the result 
+            System.out.println("Two arrays are not equal"); 
+        } 
+    } 
+>>>>>>> 24602f7 (add test1.java)
 }
